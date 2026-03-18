8b3fb9f30c8c9eca61773a44ea994049a13d2f39
Artaeon <raphael.lugmayr@stoicera.com>
2026-03-18 22:56:21 +0100

    Unify git overlay border color to use OverlayBorderColor

--- internal/tui/git.go
+++ internal/tui/git.go
@@ -398,7 +398,7 @@ func (g GitOverlay) View() string {
 
 	border := lipgloss.NewStyle().
 		BorderStyle(lipgloss.RoundedBorder()).
-		BorderForeground(green).
+		BorderForeground(OverlayBorderColor).
 		Padding(1, 2).
 		Width(width).
 		Background(mantle)
