96bde11b2b06b60c5461e4c31cdfdd547978f13e
Artaeon <raphael.lugmayr@stoicera.com>
2026-03-18 22:56:26 +0100

    Unify templates overlay border color to use OverlayBorderColor

--- internal/tui/templates.go
+++ internal/tui/templates.go
@@ -474,7 +474,7 @@ func (t Templates) View() string {
 
 	border := lipgloss.NewStyle().
 		BorderStyle(lipgloss.RoundedBorder()).
-		BorderForeground(blue).
+		BorderForeground(OverlayBorderColor).
 		Padding(1, 2).
 		Width(width).
 		Background(mantle)
